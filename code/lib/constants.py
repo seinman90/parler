9dbfc13f1beb02652321000f4a53863b030d66f5
Sarah Inman <sei2112@columbia.edu>
2021-03-09 10:36:46 -0500

    Added constants

---
+++ code/lib/constants.py
@@ -0,0 +1,6 @@
+SEED = 8338
+
+partsPath = 'data/intermediate/parlerText/inParts/'
+wholePath = 'data/intermediate/parlerText/whole/whole.csv'
+assemblyHandlingPath = 'data/intermediate/parlerText/handling/''
+partialTextPath = 'data/raw/parler_2020-01-06_posts-partial/'
