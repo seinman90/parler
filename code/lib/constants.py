cc5523b7f1f4c4eca3ef1bb747e7b58de8f806c9
Sarah Inman <sei2112@columbia.edu>
2021-03-25 07:37:41 -0400

    Updated constants file

--- code/lib/constants.py
+++ code/lib/constants.py
@@ -1,6 +1,10 @@
 SEED = 8338
 
+wd = '/home/beeb/Insync/sei2112@columbia.edu/Google Drive/Columbia SusDev PhD/Research/My work/parler/'
 partsPath = 'data/intermediate/parlerText/inParts/'
 wholePath = 'data/intermediate/parlerText/whole/whole.csv'
-assemblyHandlingPath = 'data/intermediate/parlerText/handling/''
-partialTextPath = 'data/raw/parler_2020-01-06_posts-partial/'
+assemblyHandlingPath = 'data/intermediate/parlerText/handling/'
+partialTextPath = 'data/raw/parler_2020-01-06_posts-partial/allfiles/'
+vaccinesRawPath = 'data/raw/vaccinations/NISPUF19.DAT'
+userInpath = 'data/raw/parler/users/parler_user00000000000{}.ndjson'
+userOutpath = 'data/intermediate/parler/users/parler_user00000000000{}.csv'
